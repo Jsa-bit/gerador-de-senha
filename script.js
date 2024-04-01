e78af77b4d57525f72f5c2fb1ffd6faa0fecaf7e
Jsa-bit <jefersonsimonett@gmail.com>
2024-04-01 13:08:55 -0300

    JavaScript finalizado
    Projeto gerador de senhas aleatório finalizado.

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log ("Teste js")
+let sliderElement = document.querySelector("#slider");
+let buttomElement = document.querySelector("#buttom");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvxwyzABCDEFGHIJKLMNOPQRSTUVXWYZ0123456789*#@";
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+sliderElement.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+function geradorDeSenha(){
+    let pass = "";
+
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+    containerPassword.classList.remove("hide");
+    password.innerHTML=pass;
+    novaSenha=pass;
+}
+function copyPassword(){
+    alert("Senha copiada com sucesso!")
+    navigator.clipboard.writeText(novaSenha);
+}
